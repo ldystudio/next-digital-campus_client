4a9255aae8363300cd57fe5983f9bfe5fabea9c6
Liudy <1187551003@qq.com>
2023-12-20 22:59:51 +0800

    🐎 ci(ep): 添加一些eslint与prettier的配置

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    printWidth: 100,
+    trailingComma: "none",
+    semi: false,
+    jsxSingleQuote: true,
+    tabWidth: 4,
+    useTabs: false,
+}
