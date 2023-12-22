04bcec29793bf091ce986e07e6365a483945aeb1
Liudy <1187551003@qq.com>
2023-12-22 09:18:01 +0800

    🌈 style(.env): 添加测试服务地址

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     // basePath: '',
     env: {
         // SERVER_URL: "http://192.168.1.215:8000/api",
+        // SERVER_URL: "http://10.20.98.24:8000/api",
         SERVER_URL: "http://127.0.0.1:8000/api",
         ROUTE_HOME_PATH: "/",
         TOKEN_LIFETIME: "43200"
