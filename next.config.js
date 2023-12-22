0d7760cb68151245cde8481fe383b4b4bc67b858
Liudy <1187551003@qq.com>
2023-12-22 16:45:20 +0800

    🦄 refactor(后端接口): 修改后端接口

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
     poweredByHeader: process.env.NODE_ENV !== "production",
     // basePath: '',
     env: {
-        // SERVER_URL: "http://192.168.1.215:8000/api",
-        // SERVER_URL: "http://10.20.98.24:8000/api",
-        SERVER_URL: "http://127.0.0.1:8000/api",
+        // SERVER_URL: "http://192.168.1.215:8000/api/v1",
+        // SERVER_URL: "http://10.20.98.24:8000/api/v1",
+        SERVER_URL: "http://127.0.0.1:8000/api/v1",
         ROUTE_HOME_PATH: "/",
         TOKEN_LIFETIME: "43200"
     },
