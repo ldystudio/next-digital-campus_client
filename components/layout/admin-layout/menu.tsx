becfce27e20c81fdf72bde52584fbe7e75b16b4d
Liudy <1187551003@qq.com>
2023-12-25 17:55:30 +0800

    🦄 refactor(菜单): 动态菜单会重复刷新，改写为固定菜单

--- components/layout/admin-layout/menu.tsx
+++ components/layout/admin-layout/menu.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import { memo } from "react"
-
 import { useTheme } from "next-themes"
 import { useIsClient } from "usehooks-ts"
 
@@ -16,10 +14,6 @@ import {
 import { useMenuItemAction, useMenuItemState } from "~/store/modules/menu"
 import { getMenus } from "~/store/modules/route/helpers"
 import { useRouterPush } from "~/utils/router"
-
-interface AccordionProps {
-    items: App.AdminMenu[]
-}
 
 export default function AdminMenu() {
     const { setMenuItem } = useMenuItemAction()
@@ -41,37 +35,38 @@ export default function AdminMenu() {
         )
     }
 
+    const itemClasses = {
+        base: "py-0 w-full",
+        title: "font-normal text-medium hover:text-primary",
+        trigger: "px-2 py-0 hover:bg-default-100 rounded-lg h-10 flex items-center",
+        indicator: "text-medium"
+    }
+
+    function hasChildren(item: App.AdminMenu) {
+        return item.children && item.children.length > 0
+    }
+
     function handlePress(item: App.AdminMenu) {
         setMenuItem(item)
         routerPush(item.routePath as AuthRoute.RoutePath)
     }
 
-    const MenuAccordion = memo(function Accordion({ items }: AccordionProps) {
-        // 是否有子菜单
-        function hasChildren(item: App.AdminMenu) {
-            return item.children && item.children.length > 0
-        }
-
-        return (
+    return (
+        <ScrollShadow className='h-full w-full' size={20} hideScrollBar>
             <NextUiAccordion
                 showDivider={false} // 不显示分割符
-                itemClasses={{
-                    base: "py-0 w-full",
-                    title: "font-normal text-medium hover:text-primary",
-                    trigger: "px-2 py-0 hover:bg-default-100 rounded-lg h-10 flex items-center",
-                    indicator: "text-medium"
-                }}
-                defaultExpandedKeys={[`AccordionItem - ${menuItem.key}`]} // 默认打开第一个
+                itemClasses={itemClasses}
+                defaultExpandedKeys={[`${menus[0].key}`]} // 默认打开第一个
                 // selectionMode='multiple'
             >
-                {items.map((item: any) => (
+                {menus.map((item) => (
                     <AccordionItem
+                        key={item.key}
                         aria-label={item.label}
-                        key={`AccordionItem - ${item.key}`}
                         title={item.label}
                         startContent={
                             <Icon
-                                icon={item.icon}
+                                icon={item.icon || ""}
                                 color={
                                     item.label === menuItem.label
                                         ? "#006FEE"
@@ -90,16 +85,89 @@ export default function AdminMenu() {
                             !hasChildren(item) && handlePress(item)
                         }}
                     >
-                        {hasChildren(item) && <MenuAccordion items={item.children} />}
+                        {item.children && (
+                            <NextUiAccordion showDivider={false} itemClasses={itemClasses}>
+                                {item.children.map((subItem) => (
+                                    <AccordionItem
+                                        key={subItem.key}
+                                        aria-label={subItem.label}
+                                        title={subItem.label}
+                                        startContent={
+                                            <Icon
+                                                icon={subItem.icon || ""}
+                                                color={
+                                                    subItem.label === menuItem.label
+                                                        ? "#006FEE"
+                                                        : theme === "light"
+                                                          ? "#11181C"
+                                                          : "#FAFAFA"
+                                                }
+                                                height='auto'
+                                            />
+                                        }
+                                        hideIndicator
+                                        classNames={{ content: "hidden" }}
+                                        onPress={() => {
+                                            handlePress(subItem)
+                                        }}
+                                    />
+                                ))}
+                            </NextUiAccordion>
+                        )}
                     </AccordionItem>
                 ))}
             </NextUiAccordion>
-        )
-    })
-
-    return (
-        <ScrollShadow className='h-full w-full' size={20} hideScrollBar>
-            <MenuAccordion items={menus} />
         </ScrollShadow>
     )
 }
+
+// const MenuAccordion = memo(function Accordion({ items }: AccordionProps) {
+//     // 是否有子菜单
+//     function hasChildren(item: App.AdminMenu) {
+//         return item.children && item.children.length > 0
+//     }
+
+//     return (
+//         <NextUiAccordion
+//             showDivider={false} // 不显示分割符
+//             itemClasses={{
+//                 base: "py-0 w-full",
+//                 title: "font-normal text-medium hover:text-primary",
+//                 trigger: "px-2 py-0 hover:bg-default-100 rounded-lg h-10 flex items-center",
+//                 indicator: "text-medium"
+//             }}
+//             defaultExpandedKeys={[`${items[0].key}`]} // 默认打开第一个
+//             // selectionMode='multiple'
+//         >
+//             {items.map((item: App.AdminMenu) => (
+//                 <AccordionItem
+//                     aria-label={item.label}
+//                     key={item.key}
+//                     title={item.label}
+//                     startContent={
+//                         <Icon
+//                             icon={item.icon || ""}
+//                             color={
+//                                 item.label === menuItem.label
+//                                     ? "#006FEE"
+//                                     : theme === "light"
+//                                       ? "#11181C"
+//                                       : "#FAFAFA"
+//                             }
+//                             height='auto'
+//                         />
+//                     }
+//                     hideIndicator={!hasChildren(item)}
+//                     classNames={{
+//                         content: hasChildren(item) ? "flex" : "hidden"
+//                     }}
+//                     onPress={() => {
+//                         !hasChildren(item) && handlePress(item)
+//                     }}
+//                 >
+//                     {hasChildren(item) && <MenuAccordion items={item.children!} />}
+//                 </AccordionItem>
+//             ))}
+//         </NextUiAccordion>
+//     )
+// })
