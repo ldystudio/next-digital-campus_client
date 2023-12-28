dd47f3bfa0ffb0301db7f24fc67be33fc54f7b79
Liudy <1187551003@qq.com>
2023-12-28 17:45:28 +0800

    🎈 perf(首页): 改用客户端跳转，防止重复跳转

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from "next/navigation"
+"use client"
+
+import { useEffect } from "react"
+
+import { useRouterPush } from "~/utils/router"
 
 export default function RedirectPage() {
-    redirect("/index")
+    const { routerPush } = useRouterPush()
+
+    useEffect(() => {
+        routerPush("/index")
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 }
