06e3ca13782bff436a905da6218041f839f023c4
Liudy <1187551003@qq.com>
2024-01-04 16:14:36 +0800

    ✨ feat(prettier): 添加插件prettier-plugin-tailwindcss

--- .prettierrc.js
+++ .prettierrc.js
@@ -24,5 +24,5 @@ module.exports = {
     ],
     importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
     importOrderTypeScriptVersion: "5.0.0",
-    plugins: ["@ianvs/prettier-plugin-sort-imports"]
+    plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"]
 }
