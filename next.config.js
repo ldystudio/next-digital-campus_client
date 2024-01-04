c2174c1cc0fa1f2ccc43e7ed69b9956de681ca6a
Liudy <1187551003@qq.com>
2024-01-05 01:36:21 +0800

    🧪 test(env): 去除一个测试地址

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
     // basePath: '',
     env: {
         // SERVER_URL: "http://192.168.1.215:8000/api/v1",
-        // SERVER_URL: "http://10.20.98.24:8000/api/v1",
         SERVER_URL: "http://127.0.0.1:8000/api/v1",
         ROUTE_HOME_PATH: "/",
         TOKEN_LIFETIME: "604800",
