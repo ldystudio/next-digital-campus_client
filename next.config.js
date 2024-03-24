b8e212bbe4f1f97fd1f0e63830ef5fad7bbb28e8
Liudy <1187551003@qq.com>
2024-03-24 22:48:17 +0800

    ✨ feat(new): 2024-03-24

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isDev = process.env.NODE_ENV === "development"
 module.exports = {
     reactStrictMode: true,
-    poweredByHeader: process.env.NODE_ENV === "development",
-    // basePath: '',
+    poweredByHeader: isDev,
     env: {
-        SERVER_URL: "http://192.168.1.215:8000/api/v1",
-        // SERVER_URL: "http://127.0.0.1:8000/api/v1",
+        SERVER_URL: isDev
+            ? "http://192.168.1.215:8000/api/v1"
+            : "https://203.195.168.162/api/v1",
         ROUTE_HOME_PATH: "/",
         TOKEN_LIFETIME: "604800",
         LOCAL_STORAGE_CACHE_TIME: "604800" // 默认缓存期限为7天
