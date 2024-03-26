62ebc49fb52eabce8c0736ca4c9ba27a35f2311f
Liudy <1187551003@qq.com>
2024-03-26 18:28:03 +0800

    ✨ feat(new): 2024-03-26

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     env: {
         SERVER_URL: isDev
             ? "http://192.168.1.215:8000/api/v1"
-            : "https://203.195.168.162/api/v1",
+            : "http://203.195.168.162:8000/api/v1",
         ROUTE_HOME_PATH: "/",
         TOKEN_LIFETIME: "604800",
         LOCAL_STORAGE_CACHE_TIME: "604800" // 默认缓存期限为7天
